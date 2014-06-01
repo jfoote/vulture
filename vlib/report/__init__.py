9c2a8858415a31a6f30ba83923cd09ee1ff37f1c
Jonathan Foote <jmfoote@loyola.edu>
2014-06-01 08:08:12 -0400

    first cut at uploading individual analysis JSON files on publish

--- vlib/report/__init__.py
+++ vlib/report/__init__.py
@@ -1,6 +1,8 @@
 import boto, os, gzip
 
-def publish(analysis_dir, html_only=False):
+from vlib.analyzers.tools import call_for_each_bug
+
+def publish(analysis_dir, bug_dir, html_only=False):
     bucket = boto.connect_s3().get_bucket("vulture88")
 
     # note that order is important below:
@@ -22,16 +24,25 @@ def publish(analysis_dir, html_only=False):
         return
 
     # publish analysis data
-    # gzip big json file
-    json_path = os.path.join("%s/summary.json" % analysis_dir)
-    gzip_path = "summary.json.gz" # will be included in gzip header  :|
+    # gzip/upload summary json file
+    upload_json("%s/summary.json" % analysis_dir, "summary.json", bucket)
+
+    # gzip/upload analysis for each bug
+    call_for_each_bug(bug_dir, partial(upload_analysis, bucket), 5)
+
+def upload_analysis(bucket, bugdir):
+    upload_json("%s/vulture.json", "%s/vulture.json", bucket)
+
+def upload_json(src, dst_key, bucket)
+    json_path = os.path.join(src)
+    gzip_path = "/tmp/%s" % src  # will be included in gzip header  :|
     try:
         gzfile = gzip.open(gzip_path, 'wb')
         gzfile.write(open(json_path, 'rt').read())
         gzfile.flush()
         gzfile.close()
 
-        key = bucket.new_key("summary.json")
+        key = bucket.new_key(dst_key)
         key.set_metadata('Content-Type', 'application/json')
         # see http://stackoverflow.com/questions/477816/what-is-the-correct-json-content-type
         key.set_metadata('Content-Encoding', 'gzip')
